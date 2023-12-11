947d316dc4093eb734f5a17e513232454913510f
RWittgreffe <robin.wittgreffe@btinternet.com>
2023-12-11 12:45:46 +0000

    More cleanup

--- inecuria/api/views.py
+++ inecuria/api/views.py
@@ -4,9 +4,6 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from config.containers import container
-from inecuria.api.serialisers import request_to_failed_import
-
 
 class RootView(APIView):
     VIEW_NAME = "root"
@@ -17,13 +14,3 @@ class RootView(APIView):
             data={"hello": "world"},
             headers={"Version": str(request.version)},
         )
-
-
-class FailedImportView(APIView):
-    VIEW_NAME = "failedimport"
-
-    def put(self, request: Request) -> Response:
-        container.failed_imports_repo.create_failed_import(
-            request_to_failed_import(request)
-        )
-        return Response(status=HTTPStatus.OK)
